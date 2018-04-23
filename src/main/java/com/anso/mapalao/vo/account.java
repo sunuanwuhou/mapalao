3a17573af52bacb17c4b797120b13974607c4319
qiumeng <774697399@qq.com>
2018-04-23 18:45:47 +0800

    第三方

---
+++ src/main/java/com/anso/mapalao/vo/account.java
@@ -0,0 +1,11 @@
+package com.anso.mapalao.vo;
+
+/**
+ * @author qiumeng
+ * @Package com.anso.mapalao.vo
+ * @Description:
+ * @date 2018 2018/4/23 18:00
+ */
+public class account {
+
+}
---
+++ src/main/java/com/anso/mapalao/vo/accounts.java
@@ -0,0 +1,18 @@
+package com.anso.mapalao.vo;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * @author qiumeng
+ * @Package com.anso.mapalao.vo
+ * @Description:
+ * @date 2018 2018/4/23 17:59
+ */
+@XStreamAlias("accounts")
+public class accounts {
+
+    @XStreamAlias("APIVersion")
+    private String APIVersion;
+
+
+}
